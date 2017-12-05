439780f2c21f1e98c37c061d799cd443ce743c3b
Ryan Butler <thebutlah@gmail.com>
2017-12-04 20:53:14 -0500

    yo

--- data_processing/flatten_lemmatized.py
+++ data_processing/flatten_lemmatized.py
@@ -9,7 +9,7 @@ import sys
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from data_processing.utils import apply_over_generator
+from utils import apply_over_generator
 
 
 def main():
