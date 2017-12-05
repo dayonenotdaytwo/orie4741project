35c621c53172b6fc7aa7b906969b8441a32ef7fc
Ryan Butler <thebutlah@gmail.com>
2017-12-04 20:46:39 -0500

    Fixed pythonpath?

--- data_processing/flatten_lemmatized.py
+++ data_processing/flatten_lemmatized.py
@@ -7,7 +7,7 @@ import argparse
 import os
 import sys
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.abspath(os.path.dirname(__file__) + '../'))
 
 from data_processing.utils import apply_over_generator
 
